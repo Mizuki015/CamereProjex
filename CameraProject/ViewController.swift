79dda1d7e7f4c7b9e171c25acfa9f26ed30ef26e
城間海月 <mizushiro426426@gmail.com>
2019-11-19 14:21:40 +0800

    レイアウト完成 カメラ、アルバム、保存のボタンを作った

--- CameraProject/ViewController.swift
+++ CameraProject/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // カメラボタン
+    @IBAction func runCamera(_ sender: Any) {
+    }
+    
+    // アルバムボタン
+    @IBAction func runAlbum(_ sender: Any) {
+    }
+    
+    // 保存ボタン
+    @IBAction func savePhoto(_ sender: Any) {
+    }
+    
 }
 
